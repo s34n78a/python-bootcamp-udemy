e1d03df955551a162fd78bce10cc335c3a984127
s34n78a <patrick.sean781@gmail.com>
2026-01-25 12:00:08 +0800

    Add exercises for basic Python functions and data structures

---
+++ adv_obj_data_structure/exercise.py
@@ -0,0 +1,38 @@
+print(1024, bin(1024), hex(1024))
+
+print()
+
+print(round(5.23222,2))
+
+print()
+
+s = 'hello how are you Mary, are you feeling okay?'
+print(s.islower())
+
+print()
+
+s = 'twywywtwywbwhsjhwuwshshwuwwwjdjdid'
+print(s.count('w'))
+
+print()
+
+set1 = {2,3,1,5,6,8}
+set2 = {3,1,7,5,6,8}
+print(set1.intersection(set2))
+
+print()
+
+d = {x:x**3 for x in range(5)}
+print(d)
+
+print()
+
+list1 = [1,2,3,4]
+list1.reverse()
+print(list1)
+
+print()
+
+list2 = [3,4,2,5,1]
+list2.sort()
+print(list2)
